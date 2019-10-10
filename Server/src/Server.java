451509a87852952b8d0c17f3ee172ac1ec475b44
Robbie <rohero@live.dk>
2019-10-10 10:17:18 +0200

    Server update
    A few things to test out. Not working, yet.

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -5,18 +5,77 @@ public class Server {
 
 	public static void main(String[] args) throws IOException {
 		ServerSocket ss = new ServerSocket(25565);
-		Socket s = ss.accept();
+		ss.setReuseAddress(true);
+		//Note: Important. Allows socket to be bound, even if other is in timeout state.
 		
-		System.out.println("Success");
 		
-
-		InputStreamReader input = new InputStreamReader(s.getInputStream());
-		BufferedReader bfr = new BufferedReader(input);
-		
-		String inpstr = bfr.readLine();
-		System.out.println(inpstr);
+		while (true) {
+			Socket client = ss.accept();
+			System.out.println("Success");
+			//Accept incoming connections
+			
+			User userhandler = new User(client);
+			//New class object
+			
+			new Thread(userhandler).start();
+			//New thread to handle new class object, while main thread accepts connections.
+			
+		}
 		
 	}
 	
 	
-}
+	
+	
+	private static class User implements Runnable {
+		private Socket userSocket;
+		
+		public User(Socket socket) {
+			//CONSTRUCTOR
+			this.userSocket = socket;
+		}
+		
+		
+		@Override
+		public void run() {
+			InputStreamReader input = null;
+			PrintWriter output = null;
+			//Instantiate, needed later on
+			
+			try {
+				input = new InputStreamReader(userSocket.getInputStream());
+				BufferedReader bfr = new BufferedReader(input);
+				output = new PrintWriter(userSocket.getOutputStream(), true);
+
+				String inpstr = bfr.readLine();
+				System.out.println(inpstr);
+				//Test to print input from client
+				
+				String received;
+				
+				while ((received = bfr.readLine()) != null) {
+					System.out.println(received);
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			finally {
+				
+				//Close socket
+				
+                try {
+                    if (output != null) {
+                        output.close();
+                    }
+                    if (input != null)
+                        input.close();
+                    userSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+		}
+		
+	}}}
