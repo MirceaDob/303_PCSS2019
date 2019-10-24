249d01854ae85f41a8f38f7970df5b06a8b58162
MirceaD <mircea.dobre@gmail.com>
2019-10-24 19:32:34 +0200

    Update QuestionWindow.java

--- Client/src/QuestionWindow.java
+++ Client/src/QuestionWindow.java
@@ -1,23 +1,41 @@
 import javax.swing.*;
+import java.awt.Graphics;
 
 public class QuestionWindow {
 JFrame q;
+Graphics g;
+int scor;
+
 QuestionWindow(){
 	q=new JFrame();
 	
-	JButton a1=new JButton("A1");
-	a1.setBounds(150,100,100,40);
+	JTextArea score=new JTextArea("Score:", scor, scor);
+	score.setBounds(400,50,200,100);
+	score.setEditable(false);
+	q.add(score);
+	
+	JTextArea question=new JTextArea("Question");
+	question.setBounds(100,250,800,200);
+	question.setEditable(false);
+	q.add(question);
+	
+	JButton next=new JButton("Next Question");
+	next.setBounds(700,75,150,40);
+	q.add(next);
+		
+	JButton a1=new JButton("Answer 1");
+	a1.setBounds(50,500,900,40);
 	q.add(a1);
 	
-	JButton a2=new JButton("A2");
-	a2.setBounds(150,100,100,40);
+	JButton a2=new JButton("Answer 2");
+	a2.setBounds(50,550,900,40);
 	q.add(a2);
 	
-	JButton a3=new JButton("A3");
-	a3.setBounds(150,100,100,40);
+	JButton a3=new JButton("Answer 3");
+	a3.setBounds(50,600,900,40);
 	q.add(a3);
 	
-	q.setSize(400,500);
+	q.setSize(1000,700);
 	q.setLayout(null);  
 	q.setVisible(true);  
 }
