a278f7393acdd3ce618c6f5ec6d014ccbc8ade71
RichardCuper <43847786+RichardCuper@users.noreply.github.com>
2019-10-28 15:00:28 +0100

    Updated array randomizer

--- Server/src/MysqlCon.java
+++ Server/src/MysqlCon.java
@@ -1,23 +1,40 @@
 import java.sql.*;
-
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class MysqlCon {
 	public static void printSQL() {
-		
-		
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			//dedicated connection url, name and password
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/questiondata?useTimezone=true&serverTimezone=UTC","Test","root");
-			//used to implement simple SQL elements with no parameters
+			Class.forName("com.mysql.jdbc.Driver");
+			Scanner scan = new Scanner(System.in);
+			// dedicated connection url, name and password
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/questiondata", "root",
+					"Matus_2208");
+			// used to implement simple SQL elements with no parameters
 			Statement stmt = con.createStatement();
-			//this one asks for which columns in the table to print out
-			ResultSet rs = stmt.executeQuery("select ID, Qst, Opt1, Opt2, Opt3 from questiondata");
-			while (rs.next())
-				//prints out in the console the columns, number suggests the column
-				System.out.println(rs.getInt(1) + " | " + rs.getString(2) + "     A (" + rs.getString(3) +")"+ "     B (" + rs.getString(4)+")" + "     C (" + rs.getString(5)+")");
-			con.close();
+			// this one asks for which columns in the table to print out
+
+			String find = scan.next();
+			String query = "SELECT * FROM questiondata WHERE ID ='" + find + "'";
+			ResultSet rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				String ID = rs.getString("ID");
+				String qst = rs.getString("Qst");
+				String ans1 = rs.getString("Opt1");
+				String ans2 = rs.getString("Opt2");
+				String ans3 = rs.getString("Opt3");
+					
+				//System.out.println(ID + " Question: " + qst);
+				//System.out.println("A - " +    ans1  +"     B - " + ans2 +"     C - " + ans3);
+				
+				String[] qna = {qst,ans1,ans2,ans3};
+				
+				System.out.println(qna[0]);
+				System.out.println(qna[1]+"  " + qna[2] + "   " +qna[3] + "   ");
+				
+			}
 		} catch (Exception e) {
 			System.out.println(e);
 		}
